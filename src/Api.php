68a4cd65f7bde354eb137d12286f7890b06058e1
bruno <babirondo@gmail.com>
2018-12-23 01:07:53 +0000

    funcinoando com servico de imagens

--- src/Api.php
+++ src/Api.php
@@ -4,92 +4,152 @@ namespace babirondo\REST;
 class RESTCall
 {
 
+  function IdentaRetorno( $msg ){
+    $linhas = explode("<quebralinha>", $msg)  ;
 
+    $novaMsg = null;
+    $tabs = 1;
 
-    function CallAPI($method, $url, $data = false)
-    {
-      //  require_once("include/globais.php");
-//        $Globais = new Globais();
+    for ($i=0; $i<count($linhas);$i++){
+      if (substr($linhas[$i],0,5) == "XTID:") $tabs++;
 
-        $curl = curl_init();
-  //      if ($Globais->env =="local")
-        $verbose= 1;
-        $debug = null;
-        switch ($method)
-        {
-            case "POST":
-                curl_setopt($curl, CURLOPT_POST, 1);
-
-                if ($data){
-                    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-                }
-                if ($verbose) $debug.= " <BR><FONT COLOR='red'> curl -H 'Content-Type: application/json' -X $method -d '$data' $url </FONT>";
-
-            break;
-            case "PUT":
-                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-                curl_setopt($curl, CURLOPT_POSTFIELDS,http_build_query(json_decode($data)));
-
-                if ($verbose) $debug .=  " <BR><FONT COLOR='green'> curl -H 'Content-Type: application/json' -X $method -d ' $data' $url </></FONT> ";
-                break;
-
-            case "DELETE":
-                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
-
-                if ($verbose) $debug .=  " <BR><FONT COLOR='green'> curl -H 'Content-Type: application/json' -X $method -d '$data' $url </FONT> ";
-                break;
-
-            default:
-                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
-                if ($verbose) $debug.=  " <BR> <FONT COLOR='#9acd32'>   $url </FONT> ";
-                if ($data) $url = sprintf("%s?%s", $url, http_build_query($data));
-
-        }
-
-        try {
-            ini_set('display_errors', '1');
-
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_TIMEOUT, 10);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-            $inicio1 = microtime(true);
-            $result = curl_exec($curl);
-            $total1 = microtime(true) - $inicio1;
-
-            $http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-            $teste_json_result = $result;
-
-            $parseResposta = ((json_decode( $result , true))? "verdadeiro" : "falso" );
-
-            $debug = $debug."($total1)" ;
-
-            if ($http_code != 200 ||  $parseResposta == "falso" ) echo $debug." <- Curl (HTTP CODE: <font color=red>$http_code</font>) PARSE (<font color=red>$parseResposta</font>) = <TEXTAREA>".var_export($result,1)."</TEXTAREA> ";
-            else IF ($verbose == 1) echo $debug." <- Curl (HTTP CODE: $http_code): ";
-
-            if  (json_last_error() == JSON_ERROR_NONE){
-                curl_close($curl);
-                $array_retorno_api = json_decode( $result , true);
-
-                $array_retorno_api["babirondo/rest-api"]["http_code"]  = $http_code;
-                return $array_retorno_api ;
-            }
-            else{
-                $result["babirondo/rest-api"]["httpcode"] = $http_code . "erro";
-
-                curl_close($curl);
-                return  $result;
-
-            }
-
-        }
-        catch (Exception $e) {
-            echo $debug. $e->getMessage()." Exception Curl: HTTPD CODE:$http_code ";
-
-            return false;
-        }
+      $novaMsg .= str_repeat ( ">>>>" , $tabs ).  $linhas[$i];
     }
+
+    return $novaMsg;//"<FONT color=red></font>";
+  }
+
+  function CallAPI($method, $url, $data = false, $verbose='ERRO')
+  {
+      //$verbose = [NUNCA, SEMPRE, ERRO];
+      $curl = curl_init();
+      $debug = null;
+
+
+      $data_validado =  $data;
+      switch ($method)
+      {
+          case "POST":
+              curl_setopt($curl, CURLOPT_POST, 1);
+
+              if ($data){
+                  curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+                  curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+              }
+              $debug.= "curl -H 'Content-Type: application/json' -X $method -d '$data' $url ";
+
+          break;
+
+          case "PUT":
+              curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+              curl_setopt($curl, CURLOPT_POSTFIELDS,http_build_query(json_decode($data)));
+
+              $debug .=  "curl -H 'Content-Type: application/json' -X $method -d ' $data' $url   ";
+              break;
+
+          case "DELETE":
+              curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+
+              $debug .=  "curl -H 'Content-Type: application/json' -X $method -d '$data' $url  ";
+              break;
+
+          default:
+              curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+              $debug.=  " $url   ";
+              if ($data) $url = sprintf("%s?%s", $url, http_build_query($data));
+     }
+
+      try {
+          ini_set('display_errors', '1');
+
+          curl_setopt($curl, CURLOPT_URL, $url);
+          curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+          curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+          $inicio1 = microtime(true);
+          $result = curl_exec($curl);
+          $total1 = microtime(true) - $inicio1;
+
+          $http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+          $teste_json_result = $result;
+
+          $parseResposta = ((json_decode( $result , true))? "verdadeiro" : "falso" );
+
+          /*
+          if ($http_code != 200 ||  $parseResposta == "falso" ){
+            $debug = "
+            HOST: ".gethostname ( )." <BR>
+            Endpoint: $url <BR>
+            Verb: $method<BR>
+            HTTP CODE: <font color=red>$http_code</font> <BR>
+            PARSE (<font color=red>$parseResposta</font>) <BR>
+            Tempo Execucao: ($total1) <BR>
+            Payload: <TxEXTAREA cols=90 rows=4>".   print_r ($data_validado,1)  ."</TEXTAREA> <BR>
+            Retorno da API Call: <TxEXTAREA cols=90 rows=4>".print_r($result,1)."</TEXTAREA><BR>
+            Comando: <TxEXTAREA cols=90 rows=1>$debug</TEXTAREA>";
+          }
+          else{
+            $debug = "
+            HOST: ".gethostname ( )." <BR>
+            Endpoint: $url <BR>
+            Verb: $method<BR>
+            HTTP CODE: <font color=red>$http_code</font> <BR>
+            PARSE (<font color=red>$parseResposta</font>) <BR>
+            Tempo Execucao: ($total1) <BR>
+            Payload: <TxEXTAREA cols=90 rows=4>".   print_r ($data_validado,1)  ."</TEXTAREA> <BR>
+            Retorno da API Call: <TxEXTAREA cols=90 rows=4>".print_r($result,1)."</TEXTAREA><BR>
+            Comando: <TxEXTAREA cols=90 rows=1>$debug</TEXTAREA>";
+         }
+         */
+
+         $result_identado = $this->IdentaRetorno( $result );
+
+
+         $debug = "
+         <quebralinha>XTID:".uniqid()."<BR>
+         <quebralinha>HOST: ".gethostname ( )." <BR>
+         <quebralinha>Endpoint: $url <BR>
+         <quebralinha>Verb: $method<BR>
+         <quebralinha>HTTP CODE: <font color=red>$http_code</font> <BR>
+         <quebralinha>PARSE (<font color=red>$parseResposta</font>) <BR>
+         <quebralinha>Tempo Execucao: ($total1) <BR>
+         <quebralinha>Comando: <TxEXTAREA cols=90 rows=1>$debug</TEXTAREA><BR>
+         <quebralinha>Payload: <TxEXTAREA cols=90 rows=4>".   print_r ($data_validado,1)  ."</TEXTAREA> <BR>
+         <quebralinha>Retorno da API Call:<BR> <TxEXTAREA cols=90 rows=4>".print_r($result_identado,1)."</TEXTAREA>";
+
+
+
+//ECHO  "<br>HTTP CODE $http_code -- PARSE $parseResposta  --- VERBOSE $verbose ";
+
+         if ( ($http_code != 200 ||  $parseResposta == "falso" && ($verbose == "ERRO" || $verbose == "SEMPRE" )) || $verbose == "SEMPRE" )
+          echo "\n <BR><BR> $debug <BR>";
+
+          if  (json_last_error() == JSON_ERROR_NONE){
+             //SUCESSO
+              curl_close($curl);
+              $array_retorno_api = json_decode( $result , true);
+
+              $array_retorno_api["babirondo/rest-api"]["http_code"]  = $http_code;
+              $retorno  = $array_retorno_api ;
+          }
+          else{
+            //DEU ERRO
+            //  $result["babirondo/rest-api"]["httpcode"] = $http_code . "erro";
+                echo "DEU ERRO NA CHAMADA DA API $verbose $http_code $parseResposta ";
+
+              curl_close($curl);
+              $retorno  = $result ;
+          }
+
+
+          return $retorno;
+      }
+      catch (Exception $e) {
+          echo $debug. $e->getMessage()." Exception Curl: HTTPD CODE:$http_code ";
+
+          return false;
+      }
+  }
 
 }
